51590970660f7a7223d5f9b9b1c2a3063b680b65
Mark Gauzza <markgauzza@gmail.com>
2014-11-01 00:44:09 -0400

    Object model

--- sketchbook/libraries/Kara/Kara.cpp
+++ sketchbook/libraries/Kara/Kara.cpp
@@ -4,8 +4,16 @@
 
 #include "Arduino.h"
 #include "Kara.h"
+#include "LinkedList.h"
 
 Card::Card(uint32_t  signature)
 {
 	_signature = signature;
+}
+
+
+
+void CardList :: addCard(Card card)
+{
+  
 }
